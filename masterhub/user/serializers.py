785a82536eff8b93e917b830569c71177fae23f7
Ivan <vanuartw@mail.ru>
2024-06-10 09:04:53 +0300

    review-average serializer

--- masterhub/user/serializers.py
+++ masterhub/user/serializers.py
@@ -82,19 +82,18 @@ class ProfileMasterSerializer(serializers.ModelSerializer):
     def get_reviews(self, obj):
         queryset = obj.reviews_profile.all()
         len_queryset = len(queryset)
+        data = {
+            'count': len_queryset,
+        }
         if len_queryset == 0:
             average_rating = 0
         else:
-
             rating_star_all = [i.get_rating_star_display() for i in queryset]
             rating_detail = {f'rating_{i}': rating_star_all.count(i) * 100 // len_queryset for i in range(1, 6)}
+            data.update(rating_detail)
             average_rating = round(sum(rating_star_all) / len_queryset, 2)
         serializer = ReviewsSerializer(queryset[:5], many=True)
-        data = {
-            'count': len_queryset,
-            'average_rating': average_rating
-        }
-        data.update(rating_detail)
+        data['average_rating'] = average_rating
         data['detail'] = serializer.data
         return data
 
